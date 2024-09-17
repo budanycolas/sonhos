7dfbc1b4882c232ea966559e50cec9cb50fb4b12
budanycolas <106027800+budanycolas@users.noreply.github.com>
2024-09-17 10:59:41 -0300

    sdfsdf

---
+++ perguntas.js
@@ -0,0 +1,151 @@
+criaCartao('Segunda Guerra Mundial'
+    
+    
+    'Quando começou e terminou a Segunda Guerra Mundial?', 'A Segunda Guerra Mundial começou em 1939 e terminou em 1945.')
+
+
+criaCartao('Segunda Guerra Mundial'
+    
+    
+    
+    'Qual evento marcou o início da Segunda Guerra Mundial?', 'A invasão da Polônia pela Alemanha em 1o de setembro de 1939.')
+
+
+
+criaCartao('Segunda Guerra Mundial'
+    
+    
+    'O que foi o Holocausto?', 'O Holocausto foi o genocídio sistemático de seis milhões de judeus pelos nazistas durante a Segunda Guerra Mundial.)
+
+
+
+criaCartao('Segunda Guerra Mundial'
+    
+    
+    'Quem foram os principais líderes dos Aliados?', 'Winston Churchill (Reino Unido), Franklin D. Roosevelt (EUA), Josef Stalin (URSS) e Charles de Gaulle (França).)
+
+
+
+criaCartao('Segunda Guerra Mundial'
+    
+    
+    
+    'Qual foi o impacto do ataque a Pearl Harbor?', 'O ataque a Pearl Harbor em 1941 levou os Estados Unidos a entrar na Segunda Guerra Mundial.')
+
+
+
+criaCartao('Segunda Guerra Mundial'
+    
+    
+    
+    'O que foi o Dia D?', 'O Dia D foi a invasão aliada da Normandia em 6 de junho de 1944, que iniciou a libertação da Europa Ocidental do controle nazista.')
+
+
+
+criaCartao('Segunda Guerra Mundial'
+    
+    
+    
+    'Quais países compunham o Eixo?', 'Alemanha, Itália e Japão formavam o Eixo durante a Segunda Guerra Mundial.')
+
+
+
+
+criaCartao('Segunda Guerra Mundial'
+    
+    
+    'O que marcou o fim da guerra na Europa?', 'A rendição incondicional da Alemanha em 8 de maio de 1945, conhecido como o Dia da Vitória na Europa (VE Day).')
+
+
+
+criaCartao('Segunda Guerra Mundial'
+    
+    
+    'Quais cidades foram atingidas por bombas atômicas?', 'Hiroshima e Nagasaki, no Japão, foram atingidas por bombas atômicas em agosto de 1945.)
+
+
+
+criaCartao('Segunda Guerra Mundial'
+    
+    
+    
+    'Qual foi o objetivo da Conferência de Yalta?', 'Os líderes dos Aliados se reuniram em Yalta, em 1945, para discutir a reorganização da Europa pós-guerra.')
+
+
+
+criaCartao('Guerra Fria'
+    
+    
+    'O que foi a Guerra Fria?', 'A Guerra Fria foi uma disputa política, ideológica e militar entre os Estados Unidos e a União Soviética, que durou de 1947 a 1991.')
+
+
+
+criaCartao('Guerra Fria'
+    
+    
+    'O que foi o Plano Marshall?', 'O Plano Marshall foi um programa dos EUA para ajudar na reconstrução econômica da Europa Ocidental após a Segunda Guerra Mundial.')
+
+
+
+
+criaCartao('Guerra Fria'
+    
+    
+    
+    'O que significou a criação da OTAN?', 'A criação da OTAN em 1949 visava uma aliança militar entre países ocidentais para defesa mútua contra possíveis agressões soviéticas.')
+
+
+
+
+criaCartao('Guerra Fria'
+    
+    
+    'O que foi o Pacto de Varsóvia?', 'O Pacto de Varsóvia foi uma aliança militar liderada pela União Soviética, formada em 1955 como resposta à OTAN.')
+
+
+
+
+criaCartao('Guerra Fria'
+    
+    
+    
+    'O que foi a Crise dos Mísseis de Cuba?', 'Foi um confronto em 1962 entre os EUA e a União Soviética sobre a instalação de mísseis nucleares soviéticos em Cuba.')
+
+
+
+
+criaCartao('Guerra Fria'
+    
+    
+    
+    'Quando ocorreu a construção do Muro de Berlim?', 'O Muro de Berlim foi construído em 1961 para separar Berlim Ocidental de Berlim Oriental e da Alemanha Oriental.')
+
+
+
+
+criaCartao('Guerra Fria'
+    
+    
+    'Qual foi o significado da Doutrina Truman?', 'A Doutrina Truman, anunciada em 1947, comprometeu os EUA a apoiar países ameaçados pelo comunismo.')
+
+
+
+criaCartao('Guerra Fria'
+    
+    
+    'Quando e como terminou a Guerra Fria?', 'A Guerra Fria terminou em 1991, com a dissolução da União Soviética e o colapso dos regimes comunistas no Leste Europeu.)
+
+
+
+criaCartao('Guerra Fria'
+    
+    
+    'O que foi a Perestroika?', 'A Perestroika foi uma série de reformas políticas e econômicas introduzidas por Mikhail Gorbachev na União Soviética na década de 1980.)
+
+
+
+criaCartao(
+    'Guerra Fria',
+    
+    'O que representou a queda do Muro de Berlim?', 'A queda do Muro de Berlim, em 1989, simbolizou o colapso do comunismo na Europa e o fim da Guerra Fria.')
+
